3160762ccef5a4c1d486a3cf75ff84a898819312
Ían Muliterno <iannimuliterno@gmail.com>
2025-02-14 09:44:34 -0300

    MLP with ngram to predict next character

---
+++ improv_MLP.R
@@ -0,0 +1,28 @@
+library(stringr)
+
+nomes <- readr::read_csv("https://dfalbel.github.io/llm-do-zero/assets/nomes.csv")$nome
+
+nomes <- tolower(nomes)
+
+ngrams <- function(nomes, n) {
+  ngrams <- list()
+  for (nome in nomes) {
+    nome <- paste(c(rep(".", n - 1), nome, "."), collapse = "")
+    nome <- stringr::str_split_1(nome, "")
+    while(length(nome) >= n) {
+      ngrams[[length(ngrams) + 1]] <- nome[1:n] 
+      nome <- nome[-1]
+    }
+  }
+  ngrams
+}
+
+c <- 3
+dataset <- ngrams(nomes, n = c + 1)
+dataset[1:5]
+
+X <- do.call(rbind, dataset)
+ys <- X[, ncol(X)] # a última coluna é a que queremos prever
+xs <- X[, -ncol(X)] # removes a última coluna aqui
+
+xs[1:5,]
